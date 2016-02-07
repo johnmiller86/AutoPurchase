87eeb5b42095957645b06ef099b0e53e8e912f76
johnmiller86 <chefjohn2006@hotmail.com>
2016-02-06 19:16:24 -0500

    Initial commit

--- app/src/main/java/com/twh5257_jdm5908_bw/ist402/autopurchase/LoanSummaryActivity.java
+++ app/src/main/java/com/twh5257_jdm5908_bw/ist402/autopurchase/LoanSummaryActivity.java
@@ -1,7 +1,10 @@
 package com.twh5257_jdm5908_bw.ist402.autopurchase;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 public class LoanSummaryActivity extends AppCompatActivity {
 
@@ -9,5 +12,22 @@ public class LoanSummaryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.loansummary_layout);
+        TextView monthlyPayET = (TextView) findViewById(R.id.textView2);
+        TextView loanReportET = (TextView) findViewById(R.id.textView3);
+
+        // Pass data
+        Intent intent = getIntent();
+
+        String report;
+        report = intent.getStringExtra("LoanReport");
+
+        String monthlyPay;
+        monthlyPay = intent.getStringExtra("MonthlyPayment");
+        monthlyPayET.setText(monthlyPay);
+        loanReportET.setText(report);
+    }
+
+    public void goDataEntry(View view){
+        finish();
     }
 }
